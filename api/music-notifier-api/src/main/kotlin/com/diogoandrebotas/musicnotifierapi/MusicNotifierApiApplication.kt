2e36179ff3fda41e107d4606827aa1dcf29c43aa
DiogoAndreBotas <gamebreak24@gmail.com>
2022-09-04 19:23:03 +0100

    Add barebones Kotlin/Spring project

---
+++ api/music-notifier-api/src/main/kotlin/com/diogoandrebotas/musicnotifierapi/MusicNotifierApiApplication.kt
@@ -0,0 +1,11 @@
+package com.diogoandrebotas.musicnotifierapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MusicNotifierApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<MusicNotifierApiApplication>(*args)
+}
