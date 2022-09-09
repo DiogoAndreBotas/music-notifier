11c34fa1d43846dc10ad4ff08b7869a9643f6f61
DiogoAndreBotas <gamebreak24@gmail.com>
2022-09-09 20:24:47 +0100

    Add dependencies
    - Add required dependencies
    - Delete generated Spring test file

--- api/music-notifier-api/build.gradle.kts
+++ api/music-notifier-api/build.gradle.kts
@@ -1,10 +1,16 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+val ktorVersion = "2.1.0"
+val springBootVersion = "2.7.3"
+
 plugins {
     id("org.springframework.boot") version "2.7.3"
     id("io.spring.dependency-management") version "1.0.13.RELEASE"
+
     kotlin("jvm") version "1.6.21"
     kotlin("plugin.spring") version "1.6.21"
+    kotlin("kapt") version "1.7.10"
+    kotlin("plugin.serialization") version "1.7.10"
 }
 
 group = "com.diogoandrebotas"
@@ -16,13 +22,32 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.springframework.boot:spring-boot-starter-data-mongodb:$springBootVersion")
+    implementation("org.springframework.boot:spring-boot-starter-security:$springBootVersion")
+    implementation("org.springframework.boot:spring-boot-starter-web:$springBootVersion")
+    implementation("org.springframework.boot:spring-boot-configuration-processor:$springBootVersion")
+    implementation("org.springframework.session:spring-session-core")
+    implementation("org.springframework.session:spring-session-data-mongodb")
+
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.3")
+
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("de.flapdoodle.embed:de.flapdoodle.embed.mongo")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.0")
+
+    implementation("io.ktor:ktor-client-cio:$ktorVersion")
+    implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
+
+    implementation("io.jsonwebtoken:jjwt:0.9.1")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test:2.7.3")
+
+    // testImplementation("de.flapdoodle.embed:de.flapdoodle.embed.mongo")
+
+    runtimeOnly("io.ktor:ktor-client-core:$ktorVersion")
+
+    kapt("org.springframework.boot:spring-boot-configuration-processor")
 }
 
 tasks.withType<KotlinCompile> {
