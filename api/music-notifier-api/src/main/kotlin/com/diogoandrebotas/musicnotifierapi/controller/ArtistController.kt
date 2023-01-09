2943d92b5db9488dd5d942f35194884b8ef6d191
DiogoAndreBotas <gamebreak24@gmail.com>
2023-01-09 22:03:47 +0000

    Modify ArtistController
    - Change URLs
    - Add temporary endpoint to test CRON job on demand (while no integration testing is available)

--- api/music-notifier-api/src/main/kotlin/com/diogoandrebotas/musicnotifierapi/controller/ArtistController.kt
+++ api/music-notifier-api/src/main/kotlin/com/diogoandrebotas/musicnotifierapi/controller/ArtistController.kt
@@ -1,21 +1,27 @@
 package com.diogoandrebotas.musicnotifierapi.controller
 
+import com.diogoandrebotas.musicnotifierapi.cron.ScheduledJobs
 import com.diogoandrebotas.musicnotifierapi.service.ArtistService
 import com.diogoandrebotas.musicnotifierapi.service.UserService
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
-class ArtistController(val artistService: ArtistService, val userService: UserService) {
+class ArtistController(
+    val artistService: ArtistService,
+    val userService: UserService,
+    val scheduledJobs: ScheduledJobs
+) {
 
-    @PostMapping("/subscribe/artist/{artistName}")
+    @PostMapping("/subscribe/{artistName}")
     fun subscribeToArtist(@PathVariable artistName: String)
-        = artistService.upsertArtist(artistName, userService.getLoggedInUser())
+        = artistService.subscribeToArtist(artistName, userService.getLoggedInUser())
 
-    @DeleteMapping("/unsubscribe/artist/{artistName}")
+    @DeleteMapping("/unsubscribe/{artistName}")
     fun unsubscribeFromArtist(@PathVariable artistName: String)
         = artistService.unsubscribeUserFromArtist(artistName, userService.getLoggedInUser())
+
+    // Exists temporarily to test CRON job using REST API calls
+    @GetMapping("/releases")
+    fun checkForNewArtistReleases() = scheduledJobs.checkForNewArtistReleases()
 
 }
